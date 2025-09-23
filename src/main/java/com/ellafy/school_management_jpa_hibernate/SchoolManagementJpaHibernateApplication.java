52ece555237ac023342dc6e876cc949d142acfc4
OmarHamd11 <omarellafy1@gmail.com>
2025-09-23 16:31:21 +0300

    Initial commit

---
+++ src/main/java/com/ellafy/school_management_jpa_hibernate/SchoolManagementJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ellafy.school_management_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementJpaHibernateApplication.class, args);
+	}
+
+}
